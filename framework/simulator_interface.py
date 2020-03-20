2452dc266233aa920559725f751becb4200c9e09
Moshe <moshec40@gmail.com>
2020-03-20 13:38:13 +0200

    * add simulator interface class

--- framework/simulator_interface.py
+++ framework/simulator_interface.py
@@ -1,1 +1,44 @@
 
+import numpy as np
+from abc import ABC, abstractmethod
+
+from framework.action import Action
+from framework.action_type import ActionType
+from framework.state import State
+
+
+class SimulatorInterface(metaclass=ABC):
+    """
+    Environment simulator interface.
+    """
+
+    @abstractmethod
+    def reset(self) -> State:
+        """
+        Resets the environment and returns the initial state.
+        :return:
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def act(self, action: Action) -> np.float64:
+        """
+        Simulates the specified action and returns the reward signal.
+        :return:
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_state(self) -> State:
+        """
+        Returns environment's current state.
+        :return:
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_actions(self) -> ActionType:
+        """
+        Returns action type configuration.
+        """
+        raise NotImplementedError()
